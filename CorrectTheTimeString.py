04cefd6750a66e75b32431f662e75cfad4c1ef4a
Niklas <niklasch1999@gmail.com>
2023-02-10 20:50:22 +0100

    Added Correct the time string

---
+++ CorrectTheTimeString.py
@@ -0,0 +1,30 @@
+import re
+
+def time_correct(t):
+    if not t:
+        return t
+    
+    if not re.match('\d\d:\d\d:\d\d$', t):
+        return
+
+    h = int(t.split(':')[0])
+    m = int(t.split(':')[1])
+    s = int(t.split(':')[2])
+
+    if h >= 24:
+        h = h % 24
+    if m >= 60:
+        m -= 60
+        h += 1
+    if s >= 60:
+        s -= 60
+        m += 1
+    
+    return "{0:0>2}:{1:0>2}:{2:0>2}".format(h, m, s)
+
+print(time_correct(None))
+print(time_correct(''))
+print(time_correct(' 1'))
+print(time_correct('001122'))
+print(time_correct('14:59:93'))
+print(time_correct('24:01:01'))
